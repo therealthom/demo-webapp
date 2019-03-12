2f597e50bd6ac7c0f2f976eb64c79a2e0f1e21d1
Tomas Moreno <the.real.thom@gmail.com>
2019-03-12 12:20:47 -0600

    Initial commit

---
+++ src/test/java/com/spyf/demowebapp/DemoWebappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spyf.demowebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoWebappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
